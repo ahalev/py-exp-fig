20d632e2fef4c0e03244cb4fbf8d0a40ccbc3da9
avishaihalev <avishai.halev@external.total.com>
2023-02-02 17:09:05 -0800

    better exception

--- src/expfig/fig.py
+++ src/expfig/fig.py
@@ -140,6 +140,7 @@ def _config_from_yaml(file_path):
     loaded_contents = yaml.safe_load(contents)
 
     if not isinstance(loaded_contents, dict):
-        raise ValueError(f'Contents of file ({file_path}) do not deserialize into a dict.')
+        raise ValueError(f'Contents of file "{file_path}" deserialize into object of type {type(loaded_contents)},'
+                         f'not dict.')
 
     return loaded_contents
