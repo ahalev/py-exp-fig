a3fd018ffa0114f0d2f0c74182317799b9e2c356
avishaihalev <avishai.halev@external.total.com>
2023-02-13 17:43:01 -0800

    allow make subdirs

--- src/expfig/logger/log_dir.py
+++ src/expfig/logger/log_dir.py
@@ -1,7 +1,7 @@
 import os
 
 
-def make_sequential_log_dir(log_dir, use_existing_dir=False):
+def make_sequential_log_dir(log_dir, subdirs=(), use_existing_dir=False):
     """Taken from rlworkgroup/garage.
 
     Creates log_dir, appending a number if necessary.
@@ -11,6 +11,7 @@ def make_sequential_log_dir(log_dir, use_existing_dir=False):
 
     Args:
         log_dir (str): The log directory to attempt to create.
+        subdirs (list of str): subdirectories to create in the log_dir directory.
         use_existing_dir (bool): whether to simply return the dir if it exists (will log to existing dir).
 
     Returns:
@@ -26,9 +27,18 @@ def make_sequential_log_dir(log_dir, use_existing_dir=False):
                 possible_log_dir = '{}_{}'.format(log_dir, i)
                 os.makedirs(possible_log_dir)
                 log_dir = possible_log_dir
+
+            for subdir in subdirs:
+                os.makedirs(os.path.join(log_dir, subdir))
+
             return log_dir
+
         except FileExistsError:
             if use_existing_dir:
+
+                for subdir in subdirs:
+                    os.makedirs(os.path.join(log_dir, subdir), exist_ok=True)
+
                 return log_dir
 
             i += 1
