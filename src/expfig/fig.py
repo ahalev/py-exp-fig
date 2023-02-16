32f20ca772ca9dab624479a9d6f24f7b0638f621
avishaihalev <avishai.halev@external.total.com>
2023-02-16 14:57:37 -0800

    change default to get no name

--- src/expfig/fig.py
+++ src/expfig/fig.py
@@ -18,7 +18,7 @@ DEFAULT_CONFIG_PATH = os.path.join(os.getcwd(), 'default_config.yaml')
 
 
 class Config(Namespacify):
-    def __init__(self, config=None, keys_for_name=('algo', 'type'), name_prefix='', default=DEFAULT_CONFIG_PATH):
+    def __init__(self, config=None, keys_for_name=(), name_prefix='', default=DEFAULT_CONFIG_PATH):
         self.default_config = DefaultConfig(self._parse_default(config, default)).with_name_from_keys(*keys_for_name, prefix=name_prefix)
 
         self.verbosity = 0
