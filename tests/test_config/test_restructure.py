e4c6a8c0db971317961487b0cf64b3e7c557f565
avishaihalev <avishai.halev@external.total.com>
2023-04-12 15:19:02 -0700

    start config tests

---
+++ tests/test_config/test_restructure.py
@@ -0,0 +1,66 @@
+import pytest
+
+from expfig.fig import restructure_arguments
+
+CONTENTS = {
+    'car': 'vroom',
+    'wheels': 4,
+    'axles': 2
+}
+
+
+class TestRestructure:
+    def test_no_restructure(self):
+        restructured = restructure_arguments(CONTENTS)
+
+        assert restructured == CONTENTS
+
+    def test_single_key(self):
+        contents = {'jeep.car': 'vroom'}
+        restructured = restructure_arguments(contents)
+        assert restructured == {'jeep': {'car': 'vroom'}}
+
+    def test_two_toplevel_keys(self):
+        contents = {
+            'jeep.sound': 'vroom',
+            'truck.sound': 'skirt'
+        }
+        expected = {
+            'jeep': {'sound': 'vroom'},
+            'truck': {'sound': 'skirt'}
+        }
+
+        restructured = restructure_arguments(contents)
+        assert restructured == expected
+
+    def test_two_inner_keys(self):
+        contents = {
+            'jeep.sound': 'vroom',
+            'jeep.wheels': 4
+        }
+        expected = {'jeep': {
+            'sound': 'vroom',
+            'wheels': 4
+        }}
+
+        restructured = restructure_arguments(contents)
+        assert restructured == expected
+
+    def test_combination(self):
+        contents = {
+            'jeep.sound': 'vroom',
+            'jeep.wheels': 4,
+            'truck.sound': 'skirt',
+            'truck.wheels': 18,
+            'truck.axles.length': 32,
+            'truck.axles.width': 4,
+            'porsche': 'stunning'
+        }
+        expected = {
+            'jeep': {'sound': 'vroom', 'wheels': 4},
+            'truck': {'sound': 'skirt', 'wheels': 18, 'axles': {'length': 32, 'width': 4}},
+            'porsche': 'stunning'
+        }
+
+        restructured = restructure_arguments(contents)
+        assert restructured == expected
