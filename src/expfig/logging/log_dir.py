32da4e86cabafaa99eab40ac6f3c6d3dfa073703
avishaihalev <avishai.halev@external.total.com>
2023-07-16 16:40:18 -0700

    add logger creation to make_sequential_log_dir

--- src/expfig/logging/log_dir.py
+++ src/expfig/logging/log_dir.py
@@ -1,8 +1,9 @@
 import os
 import tempfile
+from expfig.logging import get_logger
 
 
-def make_sequential_log_dir(log_dir, subdirs=(), use_existing_dir=False):
+def make_sequential_log_dir(log_dir, subdirs=(), use_existing_dir=False, logger_file=None, logging_level=10):
     """Taken from rlworkgroup/garage.
 
     Creates log_dir, appending a number if necessary.
@@ -14,6 +15,8 @@ def make_sequential_log_dir(log_dir, subdirs=(), use_existing_dir=False):
         log_dir (str or None): The log directory to attempt to create. If None, logs to a temporary directory
         subdirs (list of str): subdirectories to create in the log_dir directory.
         use_existing_dir (bool): whether to simply return the dir if it exists (will log to existing dir).
+        logger_file (str or None): If not None, creates a logger that logs to this file within the log_dir.
+        logging_level: (int or str): Level for the logger, e.g. `INFO`. See python Logging module for more info.
 
     Returns:
         str: The log directory actually created.
@@ -33,17 +36,19 @@ def make_sequential_log_dir(log_dir, subdirs=(), use_existing_dir=False):
                 os.makedirs(possible_log_dir)
                 log_dir = possible_log_dir
 
-            for subdir in subdirs:
-                os.makedirs(os.path.join(log_dir, subdir))
-
-            return log_dir
+            break
 
         except FileExistsError:
             if use_existing_dir:
-
-                for subdir in subdirs:
-                    os.makedirs(os.path.join(log_dir, subdir), exist_ok=True)
-
-                return log_dir
+                break
 
             i += 1
+
+    for subdir in subdirs:
+        os.makedirs(os.path.join(log_dir, subdir), exist_ok=True)
+
+    if logger_file:
+        logger = get_logger(level=logging_level, log_file=os.path.join(log_dir, logger_file))
+        return log_dir
+
+    return log_dir
