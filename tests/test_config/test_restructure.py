abdda0e0ac15066b4b00ce5347933185f3b55a28
avishaihalev <avishai.halev@external.total.com>
2023-08-28 21:52:04 -0700

    update/add tests

--- tests/test_config/test_restructure.py
+++ tests/test_config/test_restructure.py
@@ -9,15 +9,15 @@ CONTENTS = {
 }
 
 
-class TestRestructure:
-    def test_no_restructure(self):
-        restructured = restructure_arguments(CONTENTS)
+class TestNest:
+    def test_no_nest(self):
+        restructured = nest(CONTENTS)
 
         assert restructured == CONTENTS
 
     def test_single_key(self):
         contents = {'jeep.car': 'vroom'}
-        restructured = restructure_arguments(contents)
+        restructured = nest(contents)
         assert restructured == {'jeep': {'car': 'vroom'}}
 
     def test_two_toplevel_keys(self):
@@ -30,7 +30,7 @@ class TestRestructure:
             'truck': {'sound': 'skirt'}
         }
 
-        restructured = restructure_arguments(contents)
+        restructured = nest(contents)
         assert restructured == expected
 
     def test_two_inner_keys(self):
@@ -43,7 +43,7 @@ class TestRestructure:
             'wheels': 4
         }}
 
-        restructured = restructure_arguments(contents)
+        restructured = nest(contents)
         assert restructured == expected
 
     def test_combination(self):
@@ -62,5 +62,68 @@ class TestRestructure:
             'porsche': 'stunning'
         }
 
-        restructured = restructure_arguments(contents)
+        restructured = nest(contents)
         assert restructured == expected
+
+
+class TestUnnest:
+    def test_single_key(self):
+        nested = {'jeep': {'car': 'vroom'}}
+        unnested = unnest(nested)
+        expected_contents = {'jeep.car': 'vroom'}
+        assert unnested == expected_contents
+
+    def test_single_key_namespacify(self):
+        from expfig import Namespacify
+        nested = Namespacify({'jeep': {'car': 'vroom'}})
+        unnested = unnest(nested)
+        expected_contents = {'jeep.car': 'vroom'}
+        assert unnested == expected_contents
+
+    def test_two_toplevel_keys(self):
+        contents = {
+            'jeep': {'sound': 'vroom'},
+            'truck': {'sound': 'skirt'}
+        }
+
+        expected = {
+            'jeep.sound': 'vroom',
+            'truck.sound': 'skirt'
+        }
+
+        flat = unnest(contents)
+        assert flat == expected
+
+    def test_two_inner_keys(self):
+        contents = {'jeep': {
+            'sound': 'vroom',
+            'wheels': 4
+        }}
+
+        expected = {
+            'jeep.sound': 'vroom',
+            'jeep.wheels': 4
+        }
+
+        flat = unnest(contents)
+        assert flat == expected
+
+    def test_combination(self):
+        contents = {
+            'jeep': {'sound': 'vroom', 'wheels': 4},
+            'truck': {'sound': 'skirt', 'wheels': 18, 'axles': {'length': 32, 'width': 4}},
+            'porsche': 'stunning'
+        }
+
+        expected = {
+            'jeep.sound': 'vroom',
+            'jeep.wheels': 4,
+            'truck.sound': 'skirt',
+            'truck.wheels': 18,
+            'truck.axles.length': 32,
+            'truck.axles.width': 4,
+            'porsche': 'stunning'
+        }
+
+        flat = unnest(contents)
+        assert flat == expected
