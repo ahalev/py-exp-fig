9a8d6aab5571cc461bda0c1464ad854a2c7b7639
avishaihalev <avishai.halev@external.total.com>
2023-10-14 14:35:43 -0700

    format in colors

--- src/expfig/logging/logger.py
+++ src/expfig/logging/logger.py
@@ -1,3 +1,6 @@
+import contextlib
+import enum
+import functools
 import logging
 import os
 import sys
@@ -7,7 +10,7 @@ def get_logger(level=logging.DEBUG, log_file=None):
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
+    formatter = ColorFormatter('%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(level)
@@ -26,6 +29,50 @@ def get_logger(level=logging.DEBUG, log_file=None):
             logger.info(f'Logging to file: {os.path.abspath(log_file)}')
 
     return logger
+
+class ColorFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%', validate=True):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style, validate=validate)
+        self.formats = Formats.level_formats(self._style._fmt)
+
+    def format(self, record: logging.LogRecord) -> str:
+        with self.push_format(record.levelno):
+            return super().format(record)
+
+    @contextlib.contextmanager
+    def push_format(self, level):
+        self._init()
+        old_fmt = self._style._fmt
+        self._style._fmt = self.formats[level]
+
+        try:
+            yield
+        finally:
+            self._style._fmt = old_fmt
+
+
+class Formats(enum.Enum):
+    GRAY = "\x1b[38;20m"
+    YELLOW = "\x1b[33;20m"
+    RED = "\x1b[31;20m"
+    BOLD_RED = "\x1b[31;1m"
+    RESET = "\x1b[0m"
+
+    LEVELS = {
+        logging.DEBUG: GRAY,
+        logging.INFO: GRAY,
+        logging.WARNING: YELLOW,
+        logging.ERROR: RED,
+        logging.CRITICAL: BOLD_RED
+    }
+
+    @classmethod
+    def level_formats(cls, format_str):
+        return {level: cls.format(format_str, color) for level, color in cls.LEVELS.value.items()}
+
+    @classmethod
+    def format(cls, format_str, color):
+        return ''.join([color, format_str, cls.RESET.value])
 
 
 def check_existing_handler(new_handler, existing_handlers):
