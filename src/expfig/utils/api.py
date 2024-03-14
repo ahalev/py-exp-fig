a6636ccae751738daa378594c0bc31fcd0242485
avishaihalev <avishai.halev@external.total.com>
2024-03-13 18:40:39 -0700

    add is_list_like

--- src/expfig/utils/api.py
+++ src/expfig/utils/api.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable
+
+
 def is_dict_like(obj) -> bool:
     """
     Stolen from pandas
@@ -30,3 +33,41 @@ def is_dict_like(obj) -> bool:
         # [GH 25196] exclude classes
         and not isinstance(obj, type)
     )
+
+
+def is_list_like(obj):
+    """
+    Stolen from pandas
+
+    Check if the object is list-like.
+
+    Objects that are considered list-like are for example Python
+    lists, tuples, sets, NumPy arrays, and Pandas Series.
+
+    Strings and datetime objects, however, are not considered list-like.
+
+    Parameters
+    ----------
+    obj : The object to check.
+
+    Returns
+    -------
+    is_list_like : bool
+        Whether `obj` has list-like properties.
+
+    Examples
+    --------
+    >>> is_list_like([1, 2, 3])
+    True
+    >>> is_list_like({1, 2, 3})
+    True
+    >>> is_list_like(datetime(2017, 1, 1))
+    False
+    >>> is_list_like("foo")
+    False
+    >>> is_list_like(1)
+    False
+    """
+
+    return (isinstance(obj, Iterable) and
+            not isinstance(obj, (str, bytes)))
