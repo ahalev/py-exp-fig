25715581ec9c58d68d798d449e0a7bb610861e14
avishaihalev <avishai.halev@external.total.com>
2024-03-18 16:46:27 -0700

    add parse tests

---
+++ tests/test_parse.py
@@ -0,0 +1,31 @@
+import pytest
+
+from expfig.functions._parse import ListType, str2none
+
+
+class TestListTypeFromList:
+    def test_all_str(self):
+        list_like = 'a', 'b', 'c'
+        list_type = ListType.from_list(list_like)
+
+        assert list_type.type == str2none
+
+    def test_all_int(self):
+        list_like = 0, 4, 10
+        list_type = ListType.from_list(list_like)
+
+        assert list_type.type == int
+
+    def test_all_float(self):
+        list_like = 0.0, 4.5, 10.0
+        list_type = ListType.from_list(list_like)
+
+        assert list_type.type == float
+
+    def test_nonunique(self):
+        list_like = 'a', 0
+
+        with pytest.warns(UserWarning, match='Collecting list-like argument'):
+            list_type = ListType.from_list(list_like)
+
+        assert list_type.type == str2none
