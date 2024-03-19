25ab3b74fe8067557f89679304359d8448986dc0
avishaihalev <avishai.halev@external.total.com>
2024-03-18 17:45:48 -0700

    pytest fix

--- src/expfig/utils/get_pandas.py
+++ src/expfig/utils/get_pandas.py
@@ -1,5 +1,9 @@
 class BadPandas:
     def __getattr__(self, item):
+        if item.startswith('__'):
+            # Alleviates pytest issue with self.__unwrapped__, see https://github.com/pytest-dev/pytest/issues/5080
+            raise AttributeError(item)
+
         raise ModuleNotFoundError("Optional dependency 'pandas' not installed. "
                                   "Install with 'pip install pandas' to utilize this functionality.")
 
