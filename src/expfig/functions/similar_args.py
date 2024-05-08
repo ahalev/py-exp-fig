0bb72a625ac8615306181b9e6794b03779c477d0
avishaihalev <avishai.halev@external.total.com>
2024-05-08 01:06:00 -0700

    check for similar arguments

--- src/expfig/functions/similar_args.py
+++ src/expfig/functions/similar_args.py
@@ -4,6 +4,7 @@ nlt = '\n\t'
 
 best_matches = dict()
 
+
 def get_similar_args(unrecognized_args, known_args):
     unrecognized_options = [opt.partition('=')[0].lstrip('-') for opt in unrecognized_args if opt.startswith('--')]
 
@@ -19,7 +20,11 @@ def get_similar_args_str_fmt(unrecognized_args, known_args):
     string = [f"Unrecognized arguments '{unrecognized_args}'"]
 
     for unrecognized, matches in similar_args.items():
-        s = f"'{unrecognized}' is not a recognized argument.\n\nThe most similar arguments are{nlt}{nlt.join(matches)}"
-        string.append(s)
+        if matches:
+            s = f"'{unrecognized}' is not a recognized argument.\n\nThe most similar arguments are{nlt}{nlt.join(matches)}"
+            string.append(s)
+
+    if len(string) == 1:
+        string.append('There are no similar arguments.')
 
     return '\n\n'.join(string)
