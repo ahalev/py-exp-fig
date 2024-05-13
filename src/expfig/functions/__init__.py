196a6146b4a5ef9935bc42ae523c82acb04ed318
avishaihalev <avishai.halev@external.total.com>
2024-05-13 15:53:43 -0700

    import into init

--- src/expfig/functions/__init__.py
+++ src/expfig/functions/__init__.py
@@ -1,3 +1,4 @@
 from ._flatten import depth, unflatten, flatten, nested_dict_update
 from ._str_types import str2bool, str2none
+from .comparison import compare
 from .similar_args import get_similar_args_str_fmt
