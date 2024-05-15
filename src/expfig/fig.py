7e570f3cf6d0144dacded668b4ab8669e2f976a5
avishaihalev <avishai.halev@external.total.com>
2024-05-15 09:28:49 -0700

    redefine args_dict

--- src/expfig/fig.py
+++ src/expfig/fig.py
@@ -97,7 +97,7 @@ class Config(Namespacify):
             warn_msg = get_similar_args_str_fmt(parsed_args[1], valid_option_keys)
             warn(warn_msg)
 
-        args_dict = self._extract_verbosity(args_dict)
+        args_dict = self._extract_verbosity(parsed_args[0].__dict__)
         restructured = unflatten(args_dict)
 
         self._check_restructured(restructured, self.default_config)
