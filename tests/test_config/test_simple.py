183b750c0d88178792dc4aebc82c9da7ee77cb48
avishaihalev <avishai.halev@external.total.com>
2024-06-19 11:38:12 -0700

    import InsuranceA, InsuranceB from helpers

--- tests/test_config/test_simple.py
+++ tests/test_config/test_simple.py
@@ -9,36 +9,7 @@ from copy import deepcopy
 from unittest import mock
 from expfig import Config
 
-
-class InsuranceA(yaml.YAMLObject):
-    yaml_dumper = yaml.SafeDumper
-    yaml_loader = yaml.SafeLoader
-    yaml_tag = u'!InsuranceA'
-
-    def __init__(self, value):
-        self.value = value
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-
-        return self.value == other.value
-
-
-class InsuranceB(yaml.YAMLObject):
-    yaml_dumper = yaml.SafeDumper
-    yaml_loader = yaml.SafeLoader
-    yaml_tag = u'!InsuranceB'
-
-    def __init__(self, a=None, b=None):
-        self.a = a
-        self.b = b
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-
-        return self.a == other.a and self.b == other.b
+from tests.helpers.yaml_obj import InsuranceA, InsuranceB
 
 
 CONTENTS = {
